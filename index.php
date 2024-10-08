51f4b35a799172eeec6112835880dae26824d1c3
CURT-Elien-2326035a <zeustheplayer12@hotmail.com>
2024-10-08 09:33:26 +0200

    header fin

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include __DIR__ . '/AutoLoader.php';
 
 use blog\controllers\HomePageController;
 
-include __DIR__ . '/modules/blog/views/Fragments/header.html';
+include __DIR__ . '/modules/blog/views/Fragments/homePage.html';
 include __DIR__ . "/AutoLoader.php";
 $request_uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 if ($request_uri == '' || $request_uri == 'index.php' || $request_uri == 'http://localhost:8080/') {
