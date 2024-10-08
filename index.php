97160ad1da02b47684d7e60f9a893ca5b92f0796
CURT-Elien-2326035a <zeustheplayer12@hotmail.com>
2024-10-08 09:23:37 +0200

    iml delete

--- index.php
+++ index.php
@@ -3,10 +3,9 @@ include __DIR__ . '/AutoLoader.php';
 
 use blog\controllers\HomePageController;
 
+include __DIR__ . '/modules/blog/views/Fragments/header.html';
 include __DIR__ . "/AutoLoader.php";
-
 $request_uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
-
 if ($request_uri == '' || $request_uri == 'index.php' || $request_uri == 'http://localhost:8080/') {
 
     $homePage = new HomePageController();
