9222ef4d47c02b7f728192f4368f73b21ccf32d9
CURT-Elien-2326035a <zeustheplayer12@hotmail.com>
2024-10-10 16:18:57 +0200

    ajout des fichiers tests

--- modules/blog/models/GeoJSONModel.php
+++ modules/blog/models/GeoJSONModel.php
@@ -6,7 +6,7 @@ class GeoJSONModel
 {
     public static function litGeoJSON(): string
     {
-        $geojsonFile = '/home/jules/Téléchargements/valenicina/2019/Buildings2019_ABM.geojson';
+        $geojsonFile = 'valenicina/2019/Buildings2019_ABM.geojson';
         $geojson = file_get_contents($geojsonFile);
 
 
