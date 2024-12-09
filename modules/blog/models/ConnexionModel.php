dde0b8691cb8e4bbfa3c005eea0716751750e688
Vernagut <titouanvernagut@gmail.com>
2024-12-09 09:57:02 +0100

    inscription connexion

--- modules/blog/models/ConnexionModel.php
+++ modules/blog/models/ConnexionModel.php
@@ -40,21 +40,21 @@ class ConnexionModel
         return false;
     }
 
-//    public function verifConnexion($username, $password): bool
-//    {
-//        // Requête pour vérifier si un utilisateur existe avec ce nom et ce mot de passe
-//        $sql = "SELECT * FROM utilisateurs WHERE username = :username";
-//        $stmt = $this->db->prepare($sql);
-//        $stmt->bindParam(':username', $username);
-//
-//        // Exécution de la requête
-//        $stmt->execute();
-//        $user = $stmt->fetch();
-//
-//        // Vérification du mot de passe (en supposant qu'il est haché dans la base de données)
-//        if ($user && password_verify($password, $user['password'])) {
-//            return true;
-//        }
-//        return false;
-//    }
+    public function verifConnexion($username, $password): bool
+    {
+        // Requête pour vérifier si un utilisateur existe avec ce nom et ce mot de passe
+        $sql = "SELECT * FROM utilisateurs WHERE username = :username";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(':username', $username);
+
+        // Exécution de la requête
+        $stmt->execute();
+        $user = $stmt->fetch();
+
+        // Vérification du mot de passe (en supposant qu'il est haché dans la base de données)
+        if ($user && password_verify($password, $user['password'])) {
+            return true;
+        }
+        return false;
+    }
 }
