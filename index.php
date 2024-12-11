5e10cf9d2746dfbe0b4ffd5a86dce3d7ebdd96fe
Vernagut <titouanvernagut@gmail.com>
2024-12-11 09:17:38 +0100

    page connexion en cours

--- index.php
+++ index.php
@@ -58,57 +58,13 @@ switch ($request_uri) {
         $controller->affichePage();
         break;
     case 'connexion':
-        $controller = new ConnexionController();
-        $controller->connecter();
-        break;
-
-    case 'verifConnexion':
-        if (isset($_POST['username'], $_POST['password'])) {
-            $username = $_POST['username'];
-            $password = $_POST['password'];
-
-            $connexionModel = new ConnexionModel();
-            $success = $connexionModel->verifConnexion($username, $password);
-
-            if ($success) {
-                $_SESSION['logged'] = true;
-                header("Location: /");
-                exit();
-            } else {
-                echo "Erreur lors de la connexion.";
-            }
-        } else {
-            echo "Données de connexion manquantes.";
+        $connexionPage = new ConnexionController();
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $connexionPage::connecter($_POST);
         }
+
+        $connexionPage::affichePage();
         break;
 
-    case 'inscription':
-        $controller = new inscriptionController();
-        $controller->Inscription();
-        break;
 
-    case 'verifInscription':
-        if (isset($_POST['username'], $_POST['password'], $_POST['email'])) {
-            $username = $_POST['username'];
-            $password = $_POST['password'];
-            $email = $_POST['email'];
-
-            $inscriptionModel = new InscriptionModel();
-            $success = $inscriptionModel->verifInscription($username, $password, $email);
-
-            if ($success) {
-                $_SESSION['logged'] = true;
-                header("Location: /");
-                exit();
-            } else {
-                echo "Erreur lors de l'inscription.";
-            }
-        } else {
-            echo "Données d'inscription manquantes.";
-        }
-        break;
-
-    default:
-        echo "Page non trouvée";
-        break;
 }
--- modules/blog/controllers/ConnexionController.php
+++ modules/blog/controllers/ConnexionController.php
@@ -3,19 +3,33 @@
 namespace blog\controllers;
 
 use blog\views\ConnexionView;
+use blog\models\ConnexionModel;
+use blog\models\DbConnect;
 
 class ConnexionController
 {
-    public static function connecter(): void
+    public static function connecter(array $post): void
     {
-        session_start();
-        $view = new ConnexionView();
-        if ($view->verifierConnexion()) {
-            $_SESSION['logged'] = true;
-            header("Location: /");
+        $courriel = htmlspecialchars($post["email"]);
+        $password = htmlspecialchars($post["password"]);
+
+        $connexionModel = new ConnexionModel(new DbConnect());
+
+        if ($connexionModel->login($courriel, $password)) {
+            setcookie(
+                'courrielSiti',
+                $post["email"],
+                [
+                    'expires' => time() + 365*24*3600,
+                    'secure' => true,
+                    'httponly' => true,
+                ]
+            );
+            header("Location: /home");
             exit();
         } else {
-            $view->afficher();
+            echo "Mail ou mot de passe incorrect";
+            exit();
         }
     }
 }
