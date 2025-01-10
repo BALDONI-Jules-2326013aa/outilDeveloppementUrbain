3def805f7a69f9f408751dc9b0d0dc81dfc0b4fa
Vernagut <titouanvernagut@gmail.com>
2025-01-10 08:42:39 +0100

    responsive + mise en place de commentaire dans quelques fichiers

--- modules/blog/controllers/ConnexionController.php
+++ modules/blog/controllers/ConnexionController.php
@@ -10,6 +10,7 @@ class ConnexionController
 {
     public static function affichePage(): void
     {
+        // Démarrage de la session si elle n'est pas déjà démarrée
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
@@ -17,14 +18,17 @@ class ConnexionController
         $view->afficher();
     }
 
+    // fonction de connexion
     public static function connecter(array $post): void
     {
         $username = htmlspecialchars($post["username"] ?? '');
         $password = htmlspecialchars($post["password"] ?? '');
 
+        // Vérification des champs
         $dbConnect = new DbConnect();
         $db = $dbConnect->connect();
 
+        // Si la connexion à la base de données est établie
         if ($dbConnect->isConnected()) {
             $connexionModel = new ConnexionModel($db);
 
@@ -38,23 +42,29 @@ class ConnexionController
                         'httponly' => true,
                     ]
                 );
+                //redirection vers la page d'accueil
                 header("Location: /");
                 exit();
             } else {
+                // Si l'utilisateur n'est pas trouvé dans la base de données
                 echo "Nom d'utilisateur ou mot de passe incorrect";
                 exit();
             }
         } else {
+            // Si la connexion à la base de données n'est pas établie
             echo "Erreur de connexion à la base de données.";
             exit();
         }
     }
 
+    // fonction de déconnexion
     public static function deconnecter(): void
     {
         $connexionModel = new ConnexionModel(new DbConnect());
+        // Déconnexion
         $connexionModel->logout();
 
+        //redirection vers la page de connexion
         header("Location: /connexion");
         exit();
     }
